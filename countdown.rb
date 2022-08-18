b37d8c9f7bf0b201277fad92f2360c7b496abd2c
alex kimathi <keemalekie@gmail.com>
2022-08-18 18:19:41 +0300

    commit tree fc957f415d58f71ea546ef48f50d5461185e6c40

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(months)
+
+    while months > 0
+        if months == 12
+         return "HAPPY NEW YEAR!" 
+        else 
+          puts  "#{months} SECOND(S)!"
+        end
+        months -= 1
+    end
 end
+
+def countdown_with_sleep(count)
+    while count > 0
+        sleep count
+        puts "#{count}"
+        count -= 1
+    end
+end
+countdown_with_sleep(5)
